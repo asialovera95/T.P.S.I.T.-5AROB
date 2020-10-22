481bf1e12da3817fd0868434068ecd3d1ff7b2ff
Asia Lovera <48619341+asialovera95@users.noreply.github.com>
2020-10-22 09:28:00 +0200

    Update cifrarioVernamCorretto.py

--- Cittografia/cifrarioVernamCorretto.py
+++ Cittografia/cifrarioVernamCorretto.py
@@ -6,19 +6,19 @@ key = "itisdelpozzo"
 
 def vernam(key,message):
     message = str(message)
-    m = message.upper().replace(" ","") # Convert to upper case, remove whitespace
+    m = message.upper().replace(" ","") 
     encrypt = ""
     try:
-        key = int(key)           # if the key value is not a number, then run with key = 0
+        key = int(key)           
     except ValueError:
         key = 0
     for i in range(len(m)):
-        letter = ord(m[i])-65      # Letters now range 0-25
-        letter = (letter + key)%25 # Alphanumeric + key mod 25 = 0-25
+        letter = ord(m[i])-65      
+        letter = (letter + key)%25 
         letter +=65
         
 
-        encrypt = encrypt + chr(letter) # Concatenate message
+        encrypt = encrypt + chr(letter) 
         
     return encrypt
     
